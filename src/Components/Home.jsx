2856ae0ab84faf72fca23de1c4c2ef8dac831950
Harshmishra001 <iconharsh01@gmail.com>
2025-05-10 17:08:20 +0530

    complete

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -55,7 +55,7 @@ export default function Home() {
         {/* Social Links with 3D Effect */}
         <div className="text-white text-3xl md:text-4xl flex gap-8 mt-6">
           <SocialLink href="mailto:harsh.mishra.332003@gmail.com" iconClass="fa-solid fa-envelope" ariaLabel="Email" />
-          <SocialLink href="https://drive.google.com/file/d/1u1cHKhjFAEFqgQBOMvfbxiR-lqCtPqdw/view?usp=drivesdk" iconClass="fa-solid fa-file-lines" ariaLabel="Download Resume" />
+          <SocialLink href="https://drive.google.com/file/d/1zG840OXcw2qkx4qBulwkFuIDdf8OhbdS/view?usp=drivesdk" iconClass="fa-solid fa-file-lines" ariaLabel="Download Resume" />
           <SocialLink href="https://www.linkedin.com/in/harsh-mishra001/" iconClass="fa-brands fa-linkedin" ariaLabel="LinkedIn" />
           <SocialLink href="https://github.com/Harshmishra001" iconClass="fa-brands fa-github" ariaLabel="GitHub" />
         </div>
